26637acada525c968208c84cfee160c5e6cf03af
Nagateja <valivetiteja2004@gmail.com>
2022-07-28 21:26:29 +0530

    Commit Notes Fetch and Add 3rd Commit

--- Models/Notes.js
+++ Models/Notes.js
@@ -4,6 +4,10 @@ const { Schema } = mongoose;
 
 const notesSchema = new Schema(
     {
+        user : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'user'
+        },
         title : {
             type : String,
             required : true,
--- Routes/Notes.js
+++ Routes/Notes.js
@@ -1,9 +1,45 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+const findUser = require("../Middleware/findUser");
+const Notes = require("../Models/Notes");
+const { body, validationResult } = require("express-validator");
 
-router.get('/', function(req, res) {
-    res.json([]);
-})
+// Route 1 : Get All the Notes : GET "/api/notes/fetchallnotes"
+router.get("/fetchallnotes", findUser, async function (req, res) {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json(notes);
+});
+
+// Route 2 : Add the Notes : GET "/api/notes/addnotes"
+router.post(
+  "/addNotes",
+  findUser,
+  [
+    body("title").isLength({ min: 3 }),
+    body("description").isLength({ min: 5 }),
+  ],
+  async function (req, res) {
+    const {title,description,tag} = req.body;
+    // If there are Errors then response gives bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const note = new Notes({
+            title,description,tag ,user : req.user.id,
+        })
+        const saved = await note.save();
+        res.send(saved);
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+
+    res.json();
+  }
+);
 
 module.exports = router;
